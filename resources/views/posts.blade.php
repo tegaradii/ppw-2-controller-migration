3b18b38e02aa3dfdc56edca12c3c432925a1a34a
tegaraddi <tegaradinugroho808@gmail.com>
2024-09-10 08:14:11 +0700

    Initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post Controller</title>
+</head>
+<body>
+    <h1>POST CONTROLLER & MIGRATION</h1>
+    <p>Controller di Laravel adalah komponen inti dari model-view-controller (MVC) yang berfungsi sebagai perantara antara Model dan View.<br> Controller memiliki beberapa fungsi, di antaranya: Mengatur interaksi pengguna dengan aplikasi, Mengatur aliran data dan logika bisnis,<br>
+    Mengelola rute dengan lebih terstruktur, Membantu menjaga pemisahan masalah. Controller biasanya disimpan di direktori app/Http/Controllers dan mengikuti konvensi penamaan tertentu.<br> Setiap metode di dalam Controller merepresentasikan sebuah aksi yang dapat dilakukan oleh pengguna pada aplikasi.<br>
+    Laravel adalah framework PHP open source yang mengikuti pola desain MVC. 
+    </p>
+</body>
+</html>
